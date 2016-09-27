cdfe685e0ff355c682ff72c1fa04f737b875668f
Jonathan Tinkham <sctincman@gmail.com>
2016-09-26 21:19:32 -0600

    Remove procedures from parent webapp sample

--- src/server/db.clj
+++ src/server/db.clj
@@ -4,29 +4,6 @@
 (def db-spec {:dbtype "h2"
               :dbname "./db/server"})
 
-(defn add-location-to-db
-  [x y]
-  (let [results (j/with-db-connection [db-conn db-spec]
-                  (j/insert! db-conn
-                             :locations
-                             {:x x :y y}))]
-    (assert (= (count results) 1))
-    (first (vals (first results)))))
-
-(defn get-xy
-  [loc-id]
-  (let [results (j/with-db-connection [db-conn db-spec]
-                  (j/query db-conn
-                           ["select x, y from locations where id = ?" loc-id]))]
-    (assert (= (count results) 1))
-    (first results)))
-
-(defn get-all-locations
-  []
-  (let [results (j/with-db-connection [db-conn db-spec]
-                  (j/query db-conn
-                           ["select id, x, y from locations"]))]
-    results))
 (defn get-all-posts []
   (let [results (j/with-db-connection [db-conn db-spec]
                   (j/query db-conn
--- src/server/handlers.clj
+++ src/server/handlers.clj
@@ -8,18 +8,6 @@
   (cc/GET "/"
        []
        (views/home-page))
-  (cc/GET "/add-location"
-       []
-       (views/add-location-page))
-  (cc/POST "/add-location"
-        {params :params}
-        (views/add-location-results-page params))
-  (cc/GET "/location/:loc-id{[0-9]+}"
-       [loc-id]
-       (views/location-page loc-id))
-  (cc/GET "/all-locations"
-       []
-       (views/all-locations-page))
 
   (cc/GET "/users" []
           (views/user-list))
